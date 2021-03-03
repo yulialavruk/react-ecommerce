93a89d3f61b77a9006ae2ccebd791e9b1d64bd9f
Lavruk Yulia <lavruk_yulia@ukr.net>
2021-03-03 18:29:51 +0200

    custom checkout

--- src/components/checkout/checkout.jsx
+++ src/components/checkout/checkout.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { CartContext } from "../../context/cart-context";
 import { Layout } from "../shared/layout";
-import { StripeCheckout } from "./stripe-checkout/stripe-checkout";
+import { ShippingAddress } from "./custom-checkout/shipping-address";
+import CustomCheckout from "./custom-checkout/custom-checkout";
 import "./checkout.styles.scss";
 
 export const Checkout = () => {
-  const { itemCount, total } = useContext(CartContext);
+  const { itemCount, total, cartItems } = useContext(CartContext);
+  const [shipping, setShipping] = useState(null);
+  const addressShown = {
+    display: shipping ? "none" : "block",
+  };
 
+  const cardShown = {
+    display: shipping ? "block" : "none",
+  };
   return (
     <Layout>
       <div className="checkout">
         <h2>Checkout Summary</h2>
         <h3>{`Total Items: ${itemCount}`}</h3>
         <h4>{`Amount to Pay: $${total}`}</h4>
-        <StripeCheckout />
+        <div style={addressShown}>
+          <ShippingAddress setShipping={setShipping} />
+        </div>
+        <div style={cardShown}>
+          <CustomCheckout {...{ shipping, cartItems }} />
+        </div>
       </div>
     </Layout>
   );
