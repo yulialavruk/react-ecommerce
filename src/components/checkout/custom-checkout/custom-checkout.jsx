5bcee143f91bee6f0c4ec8da58bdfffc23ccfcb9
Lavruk Yulia <lavruk_yulia@ukr.net>
2021-03-06 14:48:41 +0200

    saved card

--- src/components/checkout/custom-checkout/custom-checkout.jsx
+++ src/components/checkout/custom-checkout/custom-checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { withRouter } from "react-router-dom";
 import {
   CardNumberElement,
@@ -7,12 +7,19 @@ import {
   useStripe,
   useElements,
 } from "@stripe/react-stripe-js";
+import { UserContext } from "../../../context/user-context";
+
 import { fetchFromAPI } from "../../../helpers";
 
 const CustomCheckout = ({ shipping, cartItems, history: { push } }) => {
+  const { user } = useContext(UserContext);
   const [processing, setProcessing] = useState(false);
   const [error, setError] = useState(null);
   const [clientSecret, setClienSecret] = useState(null);
+  const [cards, setCards] = useState(null);
+  const [payment, setPaymentCard] = useState("");
+  const [saveCard, setSavedCard] = useState(false);
+  const [paymentIntentId, setPaymentIntentId] = useState(null);
   const stripe = useStripe();
   const elements = useElements();
 
@@ -21,6 +28,20 @@ const CustomCheckout = ({ shipping, cartItems, history: { push } }) => {
       price: item.price,
       quantity: item.quantity,
     }));
+
+    if (user) {
+      const savedCards = async () => {
+        try {
+          const cardsList = await fetchFromAPI("get-payment-methods", {
+            method: "GET",
+          });
+          setCards(cardsList);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      savedCards();
+    }
 
     if (shipping) {
       const body = {
@@ -44,15 +65,21 @@ const CustomCheckout = ({ shipping, cartItems, history: { push } }) => {
         );
 
         setClienSecret(clientSecret);
+        setPaymentIntentId(id);
       };
 
       customCheckout();
     }
-  }, [shipping, cartItems]);
+  }, [shipping, cartItems, user]);
 
   const handleCheckout = async () => {
     setProcessing(true);
-
+    let si;
+    // check if user has selcted to save card
+    if (saveCard) {
+      // make to create a setup intent
+      si = await fetchFromAPI("save-payment-method");
+    }
     const payload = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
         card: elements.getElement(CardNumberElement),
@@ -61,6 +88,36 @@ const CustomCheckout = ({ shipping, cartItems, history: { push } }) => {
 
     if (payload.error) {
       setError(`Payment Failed: ${payload.error.message}`);
+    } else {
+      if (saveCard && si) {
+        // send the customers card details to be saved with stripe
+        await stripe.confirmCardSetup(si.client_secret, {
+          payment_method: {
+            card: elements.getElement(CardNumberElement),
+          },
+        });
+        push("/success");
+      } else {
+        push("/success");
+      }
+    }
+  };
+
+  const savedCardCheckout = async () => {
+    setProcessing(true);
+    // update the payment intent to incude the customer parameter
+    const { clientSecret } = await fetchFromAPI("update-payment-intent", {
+      body: { paymentIntentId },
+      method: "PUT",
+    });
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: payment,
+    });
+
+    if (payload.error) {
+      setError(`Payment Failed: ${payload.error.message}`);
+      setProcessing(false);
     } else {
       push("/success");
     }
@@ -89,8 +146,47 @@ const CustomCheckout = ({ shipping, cartItems, history: { push } }) => {
     },
   };
 
+  let cardOption;
+
+  if (cards) {
+    cardOption = cards.map((card) => {
+      const {
+        card: { brand, last4, exp_month, exp_year },
+      } = card;
+      return (
+        <option key={card.id} value={card.id}>
+          {`${brand}/ **** **** **** ${last4} ${exp_month}/${exp_year}`}
+        </option>
+      );
+    });
+    cardOption.unshift(
+      <option key="Select a card" value="">
+        Select A Card
+      </option>
+    );
+  }
+
   return (
     <div>
+      {user && cards && cards.length > 0 && (
+        <div>
+          <h4>Pay with saved card</h4>
+          <select
+            value={payment}
+            onChange={(e) => setPaymentCard(e.target.value)}
+          >
+            {cardOption}
+          </select>
+          <button
+            type="submit"
+            disabled={processing || !payment}
+            className="button is-black nomad-btn submit saved-card-btn"
+            onClick={() => savedCardCheckout()}
+          >
+            {processing ? "PROCESSING" : "PAY WITH SAVED CARD"}
+          </button>
+        </div>
+      )}
       <h4>Enter Payment Details</h4>
       <div className="stripe-card">
         <CardNumberElement
@@ -113,7 +209,16 @@ const CustomCheckout = ({ shipping, cartItems, history: { push } }) => {
           onChange={cardHandleChange}
         />
       </div>
-
+      {user && (
+        <div className="save-card">
+          <label>Save Card</label>
+          <input
+            type="checkbox"
+            checked={saveCard}
+            onChange={(e) => setSavedCard(e.target.checked)}
+          />
+        </div>
+      )}
       <div className="submit-btn">
         <button
           disabled={processing}
